b4f7195e0c1531cceb9d401061465b264b88aa05
Fabian Aiolfi <fabian.aiolfi@uzh.ch>
2019-06-24 09:04:12 +0200

    added pos_label=1 to all evals

--- python_scripts/f/G_Drive_evaluation.py
+++ python_scripts/f/G_Drive_evaluation.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
 # Get true labels
 true = []
-with open('/Users/aiolf1/Desktop/tm_miniproject_fat32/G_Drive/test_tk_upsampling.tsv', 'r') as tsvfile:
+with open('/Users/aiolf1/Desktop/tm_miniproject_fat32/G_Drive/test_tk_flair_baseline.tsv', 'r') as tsvfile:
   reader = csv.reader(tsvfile, delimiter='\t')
   for row in reader:
       # Get label as int
@@ -14,7 +14,7 @@ with open('/Users/aiolf1/Desktop/tm_miniproject_fat32/G_Drive/test_tk_upsampling
 
 # Get predicted labels
 pred = []
-with open('/Users/aiolf1/Desktop/tm_miniproject_fat32/G_Drive/test_tk_upsampling.tsv', 'r') as tsvfile:
+with open('/Users/aiolf1/Desktop/tm_miniproject_fat32/G_Drive/test_tk_flair_baseline.tsv', 'r') as tsvfile:
   reader = csv.reader(tsvfile, delimiter='\t')
   for row in reader:
       # Get label as int
@@ -29,8 +29,11 @@ print('***')
 print('Accuracy:')
 print(accuracy_score(true, pred))
 print()
-print('Precision, Recall, f-score (binary):')
+print('Precision, Recall, f-score (binary), pos_label=0 :')
 print(precision_recall_fscore_support(true, pred, pos_label=0, average='binary'))
+print()
+print('Precision, Recall, f-score (binary), pos_label=1 :')
+print(precision_recall_fscore_support(true, pred, pos_label=1, average='binary'))
 print()
 print('Precision, Recall, f-score (micro):')
 print(precision_recall_fscore_support(true, pred, average='micro'))
@@ -41,3 +44,31 @@ print()
 print('Precision, Recall, f-score (weighted):')
 print(precision_recall_fscore_support(true, pred, average='weighted'))
 print()
+
+
+# Test Data
+# test_true = [0,1,1,0,0,0]
+# test_pred = [0,1,0,0,0,0]
+
+# Test Evaluation
+# print('Accuracy:')
+# print(accuracy_score(test_true, test_pred))
+# print()
+# print('Precision, Recall, f-score (binary):')
+# print(precision_recall_fscore_support(test_true, test_pred, pos_label=0, average='binary'))
+# print()
+# print('Precision, Recall, f-score (binary), pos_label=1:')
+# print(precision_recall_fscore_support(test_true, test_pred, pos_label=1, average='binary'))
+# print()
+# print('Precision, Recall, f-score (binary):')
+# print(precision_recall_fscore_support(test_true, test_pred, labels=[1]))
+# print()
+# print('Precision, Recall, f-score (micro):')
+# print(precision_recall_fscore_support(test_true, test_pred, average='micro'))
+# print()
+# print('Precision, Recall, f-score (macro):')
+# print(precision_recall_fscore_support(test_true, test_pred, average='macro'))
+# print()
+# print('Precision, Recall, f-score (weighted):')
+# print(precision_recall_fscore_support(test_true, test_pred, average='weighted'))
+# print()
